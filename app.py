9c7aaa9ca2f4d81d48414cabcc322756d2d37d23
santhosh-bathala <58915767+santhosh-bathala@users.noreply.github.com>
2024-08-09 13:55:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from web import app
+from web import create_app
+
+# config_name = os.getenv('FLASK_CONFIG')
+config_name = 'production'
+app = create_app(config_name)
 
 if __name__ == '__main__':
     app.run(debug=True)
