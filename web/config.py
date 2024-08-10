7fe28425b28c52b2c25ed76cd95dda5ac01a2bc1
santhosh-bathala <58915767+santhosh-bathala@users.noreply.github.com>
2024-08-11 04:02:45 +0530

    Update config.py

--- web/config.py
+++ web/config.py
@@ -1,5 +1,6 @@
 SECRET_KEY = '$2b$12$F.jh9Uv1UDBC/sLGIa2UPudjqHdD8rp/kQVG1WGnsusJmT15TRIce'
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://u485156025_ssfinances:Anvi123xyz@srv1642.hstgr.io/u485156025_ssfinances'
+# SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://u485156025_ssfinances:Anvi123xyz@srv1642.hstgr.io/u485156025_ssfinances'
+SQLALCHEMY_DATABASE_URI = 'postgresql://avnadmin:AVNS_syxUV20eOwjYfAOOe7N@pg-c217c2b-santosh-ssfinaces.k.aivencloud.com:17047/defaultdb?sslmode=require'
 
 
 
